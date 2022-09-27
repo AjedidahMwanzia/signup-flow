50b79102d3f38a70a6db320d5ba092772c7a43df
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-09-27 12:05:27 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component:LoginComponent},
+  {path:'register', component:RegisterComponent},
+  {path: 'home', component:DashboardComponent},
+  {path:'', redirectTo:'login', pathMatch:"full"}
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
